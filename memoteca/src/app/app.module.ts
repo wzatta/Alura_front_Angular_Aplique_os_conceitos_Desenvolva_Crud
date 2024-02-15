ad71fc3a326220fb40754171fad4f75f9aed06e3
wzatta <wzjssa@gmail.com>
2024-02-15 19:58:22 -0300

    Primeiro commit - Cap. 01 Conhecendo o Angular - Finalizado

---
+++ memoteca/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { PensamentosComponent } from './componentes/pensamentos/pensamentos.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CabecalhoComponent,
+    PensamentosComponent,
+    RodapeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ memoteca/src/app/componentes/pensamentos/pensamentos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamentos',
+  templateUrl: './pensamentos.component.html',
+  styleUrls: ['./pensamentos.component.css']
+})
+export class PensamentosComponent {
+
+}
