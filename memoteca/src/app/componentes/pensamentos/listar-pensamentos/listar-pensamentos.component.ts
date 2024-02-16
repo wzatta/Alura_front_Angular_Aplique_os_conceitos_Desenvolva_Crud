20b809b942a6cf1838accce8138934cb2521a922
wzatta <wzjssa@gmail.com>
2024-02-16 10:47:16 -0300

    cap. 03. Navegação com roteamento - Finalizado

---
+++ memoteca/src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-pensamentos',
+  templateUrl: './listar-pensamentos.component.html',
+  styleUrls: ['./listar-pensamentos.component.css']
+})
+export class ListarPensamentosComponent implements OnInit {
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  adicionarPensamentos() {
+    alert("mudar rota")
+}
+
+}
